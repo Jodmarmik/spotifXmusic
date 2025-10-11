107a046bf5e5c440655f294fbda156105fc35c6d
Jodmarmik <155324790+Jodmarmik@users.noreply.github.com>
2025-10-11 10:26:52 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -32,8 +32,5 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [InlineKeyboardButton(text="• ᴊᴏᴅ sᴛᴜᴅɪᴏ •", url=f"https://t.me/NLTRIDE")],
-    
     ]
-    
     return buttons
